da0c52323763bf2dd69efc617873d91320bc1a8e
Bruno Netto <mir_brosa@uolinc.com>
2022-07-30 18:45:59 -0300

    Inicia estudos para desenvolvimento da terceira seção da página

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -14,7 +14,7 @@ export default function Home() {
       </div>
 
       <div className='min-h-screen bg-slate-200 flex items-center justify-center p-10'>
-        <div className='w-fullmax-w-[1400px] grid grid-cols-2'>
+        <div className='w-full max-w-[1400px] grid grid-cols-2'>
           <div className='flex justify-center'>
             <img src={teste} alt="" />
           </div>
@@ -24,6 +24,27 @@ export default function Home() {
             <p className='text-3xl leading-9 text-gray-500 mb-5'>Fico feliz que tenha chegado até minha página.</p>
             <p className='text-3xl leading-9	text-gray-500 mb-5'>Meu nome é Bruno, tenho 20 anos de idade e uma grande paixão por desenvolvimento. No final de 2019 me formei no curso técnico de Informática para Internet, foi lá que entendi o quanto a tecnologia pode ajudar e melhorar a vida das pessoas.</p>
             <p className='text-3xl leading-9 text-gray-500'>Desde então passo os meus dias estudando para que tenha um bom conhecimento.</p>
+          </div>
+        </div>
+      </div>
+
+      <div className='min-h-screen bg-slate-200 flex items-center justify-center p-10  border border-slate-900'>
+        <div className='w-full max-w-[1400px] border border-slate-900'>
+          <h2 className='text-6xl p-5 text-bold uppercase text-white bg-blue-700'>Alguns trabalhos</h2>
+
+          <div className="grid grid-cols-3 grid-rows-3 text-transparent">
+            <div className="bg-blur border border-slate-900 flex justify-center items-center hover:text-red-600 hover:scale-150">
+              <p>Teste</p>
+            </div>
+
+            <div className="border border-slate-900"></div>
+            <div className="border border-slate-900"></div>
+            <div className="border border-slate-900"></div>
+            <div className="border border-slate-900"></div>
+            <div className="border border-slate-900"></div>
+            <div className="border border-slate-900"></div>
+            <div className="border border-slate-900"></div>
+            <div className="border border-slate-900"></div>
           </div>
         </div>
       </div>
