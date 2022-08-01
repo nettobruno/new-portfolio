479a9c98d617a87b8e55e3d77f28cf17328bb204
Bruno Netto <brunonettomac@outlook.com>
2022-08-01 10:17:22 -0300

    Melhora a responsividade das duas primeiras seções

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -22,40 +22,40 @@ import imageProjectClipboard from '../assets/clipboard.jpg'
 export default function Home() {
   return (
     <>
-      <div className='min-h-screen bg-blur bg-cover bg-center bg-no-repeat flex items-center justify-center p-10'>
+      <div className='min-h-screen bg-blur bg-cover bg-center bg-no-repeat flex items-center justify-center p-2 md:p-5 lg:p-10'>
         <div className='w-full max-w-[1400px]'>
-          <h3 className='text-7xl text-white -ml-1'>Meu nome é</h3>
-          <h1 className='text-9xl text-white font-bold -ml-2 mb-6'>Bruno Netto.</h1>
-          <h2 className='text-7xl text-white'>
-            sou desenvolvedor <span className='bg-blue-700 font-bold px-4'>Front End</span>
+          <h3 className='text-4xl md:text-6xl lg:text-7xl text-white -ml-1'>Meu nome é</h3>
+          <h1 className='text-6xl md:text-8xl lg:text-9xl text-white font-bold -ml-1 md:-ml-[6px] mb-6'>Bruno Netto.</h1>
+          <h2 className='text-4xl md:text-6xl lg:text-7xl text-white'>
+            sou desenvolvedor <span className='bg-blue-700 font-bold px-2'>Front End</span>
           </h2>
         </div>
       </div>
 
-      <div className='min-h-screen bg-slate-200 flex items-center justify-center p-10'>
-        <div className='w-full max-w-[1400px] grid grid-cols-2'>
-          <div className='flex items-center'>
+      <div className='min-h-screen bg-slate-200 flex items-center justify-center p-2 md:p-5 lg:p-10'>
+        <div className='w-full max-w-[1400px] grid grid-cols-1 lg:grid-cols-2'>
+          <div className='flex items-center justify-center mb-10 lg:justify-start'>
             <img src={imageAuthor} alt='Foto do Author do Site, Bruno Netto' />
           </div>
 
-          <div className='flex justify-center flex-col pl-8 pr-8'>
-            <h3 className='text-5xl font-bold uppercase text-gray-600 mb-6'>Seja bem vindo(a)</h3>
-            <p className='text-3xl leading-9 text-gray-500 mb-5'>
+          <div className='flex justify-center flex-col px-1 lg:px-8'>
+            <h3 className='text-2xl lg:text-5xl text-center lg:text-left font-bold uppercase text-gray-600 mb-6'>Seja bem vindo(a)</h3>
+            <p className='text-2xl lg:text-3xl text-center lg:text-left leading-9 text-gray-500 mb-5'>
               Fico feliz que tenha chegado até minha página.
             </p>
-            <p className='text-3xl leading-9	text-gray-500 mb-5'>
+            <p className='text-2xl lg:text-3xl text-center lg:text-left leading-9	text-gray-500 mb-5'>
               Meu nome é Bruno, tenho 20 anos de idade e uma grande paixão por desenvolvimento. No
               final de 2019 me formei no curso técnico de Informática para Internet, foi lá que
               entendi o quanto a tecnologia pode ajudar e melhorar a vida das pessoas.
             </p>
-            <p className='text-3xl leading-9 text-gray-500'>
+            <p className='text-2xl lg:text-3xl text-center lg:text-left leading-9 text-gray-500'>
               Desde então passo os meus dias estudando para que tenha um bom conhecimento.
             </p>
           </div>
         </div>
       </div>
 
-      <div className='min-h-screen bg-slate-200 flex items-center justify-center p-10'>
+      <div className='min-h-screen bg-slate-200 flex items-center justify-center p-2 md:p-5 lg:p-10'>
         <div className='w-full max-w-[1400px]'>
           <div className='flex justify-center'>
             <h2 className='text-5xl py-2 px-20 mb-10 text-bold uppercase text-white inline-block bg-blue-700'>
@@ -166,7 +166,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className='bg-slate-200 flex items-center justify-center p-10'>
+      <div className='bg-slate-200 flex items-center justify-center p-2 md:p-5 lg:p-10'>
         <div className='w-full max-w-[1400px]'>
           <div className='flex justify-center'>
             <h2 className='text-5xl py-2 px-20 mb-10 text-bold uppercase text-white inline-block bg-blue-700'>
