843000273c89f5cf94a561f3b056c352a9301971
Bruno Netto <brunonettomac@outlook.com>
2022-08-09 21:44:14 -0300

    Corrige scroll para cima

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,8 +1,15 @@
-import { Route, Routes } from 'react-router-dom'
+import {useEffect} from 'react'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
 import Projects from './pages/Projects'
 
 export default function Router() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <Routes>
       <Route path='/' element={<Home />} />
